d3654988250789dd7aee17158bbc1da5636883b9
Bobo <yanglinbo@gmail.com>
2025-05-30 22:02:17 +0800

    feat: refactor.

--- app/user/service/internal/data/models/user.go
+++ app/user/service/internal/data/models/user.go
@@ -2,11 +2,12 @@ package models
 
 import "gorm.io/gorm"
 
+// User 用户信息表
 type User struct {
 	gorm.Model
-	UserName string `gorm:"column:username"`
-	NickName string `gorm:"column:nickname"`
-	Password string `gorm:"column:password"`
+	UserName string `gorm:"column:username;comment:'账号名'"`
+	NickName string `gorm:"column:nickname;comment:'昵称'"`
+	Password string `gorm:"column:password;comment:'登录密码'"`
 }
 
 func (u User) TableName() string {
